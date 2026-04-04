fda09db224c9f65a80d37fb1f7b7fd0ed328acda
JAVB2077 <joe.tecnm@gmail.com>
2026-04-04 14:13:14 -0600

    se prepara jwty las configuraciones globales

---
+++ src/main/java/zentry/back/api/jwt/jwtAutenticationFilter.java
@@ -0,0 +1,31 @@
+package zentry.back.api.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+//import websiters.gastroreview.service.AddressService;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+
+public class jwtAutenticationFilter {
+    
+}
