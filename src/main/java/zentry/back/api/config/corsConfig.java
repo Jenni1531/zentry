b7e0f57e60c54cdae880d6f6c7745abedd0299f3
JAVB2077 <joe.tecnm@gmail.com>
2026-07-19 00:03:29 -0600

    Actualizacion

--- src/main/java/zentry/back/api/config/corsConfig.java
+++ src/main/java/zentry/back/api/config/corsConfig.java
@@ -1,10 +1,8 @@
 package zentry.back.api.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 @SuppressWarnings("null")
 public class corsConfig {
@@ -16,7 +14,9 @@ public class corsConfig {
                 registry.addMapping("/**")
                         .allowedOriginPatterns(
                                 "http://localhost:5173",
-                                "https://*.devtunnels.ms"
+                                "https://*.devtunnels.ms",
+                                "https://zentrycommunity.tail314909.ts.net:*",
+                                "http://192.168.*.*:*"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
